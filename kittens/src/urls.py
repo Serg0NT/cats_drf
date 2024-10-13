f542b4cb5a970aede2d88d22d82006324c32b44a
Oleynik Sergey Home <Oleynik.sergey.93@gmail.com>
2024-10-13 21:38:49 +0500

    2 to 1

--- kittens/src/urls.py
+++ kittens/src/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
